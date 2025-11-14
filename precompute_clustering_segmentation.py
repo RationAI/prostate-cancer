fix: skip remaining pipeline steps temporarily and improve heatmap normalization logging

precompute_clustering_segmentation.py
@@ -374,6 +374,9 @@ def main(num_clusters: int, experiment_directory: str, clustering_algorithm: str
             artifact_subdir="xai/gradcam"
         )
         _slide_pbar.write(f"Ensured Grad-CAM TIFF is uploaded to MLflow.")
+    
+        # skip rest of the pipeline for now
+        continue
         
         # OUT_FILE_PATH_XAI_LAYERCAM = PRECOMPUTED_DATA_DIR / "xai-layercam" / f"{slide_name}.npy"
         # OUT_FILE_PATH_XAI_LAYERCAM_TIFF = TMP_DIR / "xai-layercam" / f"{slide_name}.tiff"
@@ -685,10 +688,12 @@ def main(num_clusters: int, experiment_directory: str, clustering_algorithm: str
 
 def normalize_highlight_heatmap(xai_layercam_assembled_wsi):
     _max = np.max(np.abs(xai_layercam_assembled_wsi))
-    logger.debug(f"Layer-CAM max value before normalization: {_max}")
+    logger.debug(f"Heatmap min/max before normalization: {xai_layercam_assembled_wsi.min()}/{xai_layercam_assembled_wsi.max()}")
     np.power(xai_layercam_assembled_wsi, 1./3., out=xai_layercam_assembled_wsi)   # normalize between 0 and 1
     xai_layercam_assembled_wsi /= (_max * 2.0)
     xai_layercam_assembled_wsi += 0.5
+    xai_layercam_assembled_wsi = np.clip(xai_layercam_assembled_wsi, 0.0, 1.0)
+    logger.debug(f"Heatmap min/max after normalization: {xai_layercam_assembled_wsi.min()}/{xai_layercam_assembled_wsi.max()}")
     xai_layercam_assembled_wsi *= 255.0
     return xai_layercam_assembled_wsi
 