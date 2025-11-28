51006d10765003c9612c16820052beb173dff73b
Adam Džadoň <524839@mail.muni.cz>
2025-11-28 23:27:53 +0100

    feat: postprocessing

--- prostate_cancer/callbacks/__init__.py
+++ prostate_cancer/callbacks/__init__.py
@@ -1,6 +1,9 @@
 from prostate_cancer.callbacks.aggregator_callback import AggregatorCallback
 from prostate_cancer.callbacks.curves_callback import CurvesCallback
 from prostate_cancer.callbacks.heatmap_callback import HeatmapCallback
+from prostate_cancer.callbacks.kernel_estimation_callback import (
+    KernelEstimationCallback,
+)
 from prostate_cancer.callbacks.nested_metrics_callback import NestedMetricsCallback
 from prostate_cancer.callbacks.num_positive_callback import NumPositiveCallback
 from prostate_cancer.callbacks.tile_histograms_callback import TileHistogramsCallback
@@ -10,6 +13,7 @@ __all__ = [
     "AggregatorCallback",
     "CurvesCallback",
     "HeatmapCallback",
+    "KernelEstimationCallback",
     "NestedMetricsCallback",
     "NumPositiveCallback",
     "TileHistogramsCallback",
