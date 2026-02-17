93dc8b499baa937c343a5030a4e90cf7767e425d
Adam Džadoň <524839@mail.muni.cz>
2026-02-17 10:18:02 +0000

    fix: merge table

--- postprocessing/slide_prediction_table_merge.py
+++ postprocessing/slide_prediction_table_merge.py
@@ -8,6 +8,8 @@ from omegaconf import DictConfig
 from rationai.mlkit import autolog, with_cli_args
 from rationai.mlkit.lightning.loggers import MLFlowLogger
 
+from postprocessing.read_table import read_json_table
+
 
 @with_cli_args(["+postprocessing=slide_prediction_table_merge"])
 @hydra.main(config_path="../configs", config_name="postprocessing", version_base=None)
@@ -15,13 +17,26 @@ from rationai.mlkit.lightning.loggers import MLFlowLogger
 def main(config: DictConfig, logger: MLFlowLogger) -> None:
     df = None
     for model, table_uri in config.table_uris.items():
-        current = pd.read_csv(mlflow.artifacts.download_artifacts(table_uri))
+        path = mlflow.artifacts.download_artifacts(table_uri)
+        if path.endswith("json"):
+            current = read_json_table(path)
+        else:
+            current = pd.read_csv(mlflow.artifacts.download_artifacts(table_uri))
+
         if df is None:  # first iteration
             df = current
-            df = df.drop(["is_fp", "prediction", "bin_prediction"], axis=1)
 
-        df[f"{model}_pred_score"] = current["prediction"]
-        df[f"{model}_pred_binary"] = current["bin_prediction"]
+            # may not be present if not ehanced table
+            if "is_fp" in df.columns and "bin_prediction" in df.columns:
+                df = df.drop(["is_fp", config.pred_column, "bin_prediction"], axis=1)
+            else:
+                df = df.drop([config.pred_column], axis=1)
+
+        df[f"{model}_pred_score"] = current[config.pred_column]
+
+        # may not be present if not ehanced table
+        if "bin_prediction" in current.columns:
+            df[f"{model}_pred_binary"] = current["bin_prediction"]
 
     with tempfile.TemporaryDirectory() as tmpdir:
         out_path = os.path.join(tmpdir, "sl_preds_merged.csv")
