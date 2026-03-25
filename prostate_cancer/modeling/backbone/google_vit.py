3c5431aa1e1b45c6c040813b38cc3e4452cbf26a
Adam Džadoň <524839@mail.muni.cz>
2026-03-25 15:47:58 +0000

    feat: use just backbone directly

--- prostate_cancer/modeling/backbone/google_vit.py
+++ prostate_cancer/modeling/backbone/google_vit.py
@@ -1,11 +1,4 @@
-from transformers import ViTForImageClassification, ViTModel
-
+from transformers import ViTModel
 
 def google_vit() -> ViTModel:
-    return ViTForImageClassification.from_pretrained(
-        "google/vit-base-patch16-224",
-        num_labels=1,
-        id2label={0: "carcinoma"},
-        label2id={"carcinoma": 0},
-        ignore_mismatched_sizes=True,
-    ).vit
+    return ViTModel.from_pretrained("google/vit-base-patch16-224")
