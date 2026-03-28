4b89b4d958aabeaf0d6fe3f5133b0703f4351e80
Adam Džadoň <524839@mail.muni.cz>
2026-03-28 09:54:56 +0000

    fix: lint

--- exploration/panda.py
+++ exploration/panda.py
@@ -1,4 +1,4 @@
-""" This script takes alerady pre-explored CSV for different task, and keeps only information relevant for this task. """
+"""This script takes alerady pre-explored CSV for different task, and keeps only information relevant for this task."""
 
 import tempfile
 from pathlib import Path
@@ -18,15 +18,24 @@ from rationai.mlkit.lightning.loggers import MLFlowLogger
 )
 @autolog
 def main(config: DictConfig, logger: MLFlowLogger) -> None:
-    metadata = pd.read_csv(mlflow.artifacts.download_artifacts(config.full_metadata_csv_uri))
+    metadata = pd.read_csv(
+        mlflow.artifacts.download_artifacts(config.full_metadata_csv_uri)
+    )
 
-    mask = (metadata["has_annotation"] == True) & (metadata["has_segmentation"] == True) & (metadata["is_annotation_corrupted"] == False) & (metadata["is_wsi_valid"] == True)
-    metadata = metadata[ mask ]
-    metadata["carcinoma"] = metadata[ "isup_grade" ] != 0
-    metadata = metadata.drop( ["segmentation_id", "has_segmentation"], axis=1 ) # not needed for this task
+    mask = (
+        (metadata["has_annotation"])
+        & (metadata["has_segmentation"])
+        & ~(metadata["is_annotation_corrupted"])
+        & (metadata["is_wsi_valid"])
+    )
+    metadata = metadata[mask]
+    metadata["carcinoma"] = metadata["isup_grade"] != 0
+    metadata = metadata.drop(
+        ["segmentation_id", "has_segmentation"], axis=1
+    )  # not needed for this task
 
-    radboud_metadata = metadata[ metadata["data_provider"] == "radboud" ]
-    karolinska_metadata = metadata[ metadata["data_provider"] == "karolinska" ]
+    radboud_metadata = metadata[metadata["data_provider"] == "radboud"]
+    karolinska_metadata = metadata[metadata["data_provider"] == "karolinska"]
 
     with tempfile.TemporaryDirectory() as temp_dir:
         radboud_target = Path(temp_dir) / "radboud_metadata.csv"
@@ -34,8 +43,11 @@ def main(config: DictConfig, logger: MLFlowLogger) -> None:
         logger.log_artifact(str(radboud_target))
 
         karolinska_target = Path(temp_dir) / "karolinska_metadata.csv"
-        karolinska_metadata.to_csv(Path(temp_dir) / "karolinska_metadata.csv", index=False)
+        karolinska_metadata.to_csv(
+            Path(temp_dir) / "karolinska_metadata.csv", index=False
+        )
         logger.log_artifact(str(karolinska_target))
+
 
 if __name__ == "__main__":
     main()
