78dd9d8995c0ae515a56e17bfb8c2e4562fc51a3
Ubuntu <adam.dzadon@gmail.com>
2026-04-09 13:50:51 +0000

    feat: tiling v2 init

---
+++ preprocessing/tiling_v2/overlaps.py
@@ -0,0 +1,93 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Any
+
+import mlflow
+import pandas as pd
+from ratiopath.tiling import tile_overlay_overlap
+from ray.data import Dataset
+from ray.data.expressions import col
+from shapely.geometry import Polygon, box
+
+
+class Overlaper(ABC):
+    mask_storage: Path
+    columns_to_keep: set[str]
+    roi: Polygon
+
+    def __init__(
+        self,
+        roi_corners: tuple[int, int, int, int],
+        mask_name: str,
+        masks_folder: Path | None = None,
+        masks_uri: str | None = None,
+    ) -> None:
+        if (masks_folder is None and masks_uri is None) or (
+            masks_folder is not None and masks_uri is not None
+        ):
+            raise ValueError("Provide exactly one source of masks")
+
+        if masks_folder is not None:
+            self.mask_storage = masks_folder
+        else:
+            self.mask_storage = Path(mlflow.artifacts.download_artifacts(masks_uri))
+
+        self.roi = box(**roi_corners)
+        self.mask_name = mask_name
+
+    def add_mask_path_batch(self, batch: dict[str, Any]) -> dict[str, Any]:
+        df = pd.DataFrame(batch)
+
+        # assuming same name for the mask with .tiff suffix
+        batch[f"{self.mask_name}_path"] = df["path"].map(
+            lambda x: str(self.mask_storage / f"{Path(x).stem}.tiff")
+        )
+        return batch
+
+    def add_mask_path(self, tiles: Dataset) -> Dataset:
+        return tiles.map_batches(self.add_mask_path_batch)
+
+    def add_overlaps(self, tiles: Dataset) -> Dataset:
+        return tiles.with_column(
+            f"{self.mask_name}_overlap",
+            tile_overlay_overlap(
+                roi=self.roi,
+                overlay_path=col(f"{self.mask_name}_path"),
+                tile_x=col("x"),
+                tile_y=col("y"),
+                mpp_x=col("mpp_x"),
+                mpp_y=col("mpp_y"),
+            ),
+            num_cpus=1,
+            memory=4 * 1024**3,
+        )
+
+    @abstractmethod
+    def add_percentages(self, tiles: Dataset) -> Dataset: ...
+
+
+class BinaryOverlaper(Overlaper):
+    def __init__(
+        self,
+        roi_corners: tuple[int, int, int, int],
+        mask_name: str,
+        masks_folder: Path | None = None,
+        masks_uri: str | None = None,
+    ) -> None:
+        super().__init__(
+            roi_corners=roi_corners,
+            mask_name=mask_name,
+            masks_folder=masks_folder,
+            masks_uri=masks_uri,
+        )
+        self.columns_to_keep = {f"{self.mask_name}_percentage"}
+
+    def extract_foreground_percentage(self, tile: dict[str, Any]) -> dict[str, Any]:
+        tile[f"{self.mask_name}_percentage"] = tile["tissue_mask_overlap"].get(
+            "255", 0.0
+        )
+        return tile
+
+    def add_percentages(self, tiles: Dataset) -> Dataset:
+        tiles = self.add_overlaps(tiles)
+        return tiles.map(self.extract_foreground_percentage)
---
+++ preprocessing/tiling_v2/tiling_v2.py
@@ -0,0 +1,118 @@
+from functools import partial
+from typing import TYPE_CHECKING, Any
+
+import hydra
+import pandas as pd
+import ray
+from lightning.pytorch.loggers import Logger
+from omegaconf import DictConfig
+from rationai.mlkit import with_cli_args
+from rationai.mlkit.autolog import autolog
+from rationai.tiling.writers import save_mlflow_dataset
+from ratiopath.ray import read_slides
+from ratiopath.tiling import grid_tiles
+from ratiopath.tiling.utils import row_hash
+
+
+if TYPE_CHECKING:
+    from preprocessing.tiling_v2.overlaps import Overlaper
+
+
+ray.init(runtime_env={"excludes": [".git", ".venv"]})
+
+
+def tile(row: dict[str, Any]) -> list[dict[str, Any]]:
+    """Factory tile generator."""
+    return [
+        {
+            "tile_x": x,
+            "tile_y": y,
+            "path": row["path"],
+            "slide_id": row["id"],
+            "level": row["level"],
+            "tile_extent_x": row["tile_extent_x"],
+            "tile_extent_y": row["tile_extent_y"],
+            "mpp_x": row["mpp_x"],
+            "mpp_y": row["mpp_y"],
+        }
+        for x, y in grid_tiles(
+            slide_extent=(row["extent_x"], row["extent_y"]),
+            tile_extent=(row["tile_extent_x"], row["tile_extent_y"]),
+            stride=(row["stride_x"], row["stride_y"]),
+        )
+    ]
+
+
+def filter_tissue(row: dict[str, Any]) -> bool:
+    """Filter tiles to contain only tiles with some tissue."""
+    return row["tissue_roi_percentage"] > 0
+
+
+def select(row: dict[str, Any], to_keep: set[str]) -> dict[str, Any]:
+    """Filter tiles to contain only desired columns."""
+    tile = {
+        "slide_id": row["slide_id"],
+        "x": row["tile_x"],
+        "y": row["tile_y"],
+    }
+
+    for column in to_keep:
+        tile[column] = row[column]
+
+    return tile
+
+
+def carcinoma(row: dict[str, Any], df: pd.DataFrame) -> dict[str, Any]:
+    """Add slide level carcinoma label to slides table."""
+    slide_path = row["path"]
+    match = df.loc[df["slide_path"] == slide_path, "carcinoma"]
+
+    if match.empty:
+        raise ValueError("No matching carcinoma label")
+
+    row["carcinoma"] = match.iloc[0]
+    return row
+
+
+def tiling(df: pd.DataFrame, config: DictConfig) -> tuple[pd.DataFrame, pd.DataFrame]:
+    overlapers: list[Overlaper] = list(
+        hydra.utils.instantiate(config.overlapers).values()
+    )
+
+    slides = read_slides(
+        list(df["slide_path"]),
+        tile_extent=config.tile_extent,
+        stride=config.stride,
+        mpp=config.mpp,
+    )
+    slides = slides.map(row_hash, num_cpus=0.1, memory=128 * 1024**2)
+    slides = slides.map(partial(carcinoma, df=df), num_cpus=0.1, memory=128 * 1024**2)
+
+    tiles = slides.flat_map(tile, num_cpus=0.2, memory=128 * 1024**2)
+    tiles = tiles.repartition(target_num_rows_per_block=config.batch_size)
+
+    to_keep = set()
+    for overlaper in overlapers:
+        tiles = overlaper.add_mask_path(tiles)
+        tiles = overlaper.add_percentages(tiles)
+        to_keep |= overlaper.columns_to_keep
+
+    tiles = tiles.filter(filter_tissue, num_cpus=0.1, memory=128 * 1024**2)
+    tiles = tiles.map(
+        partial(select, to_keep=to_keep), num_cpus=0.1, memory=128 * 1024**2
+    )
+
+    return slides.to_pandas(), tiles.to_pandas()
+
+
+@with_cli_args(["+preprocessing=tiling_v2"])
+@hydra.main(config_path="../../configs", config_name="preprocessing", version_base=None)
+@autolog
+def main(config: DictConfig, logger: Logger | None = None) -> None:
+    df = pd.read_csv(config.data.metadata_table)
+    slides, tiles = tiling(df, config)
+    save_mlflow_dataset(slides, tiles, config.data.data_name)
+
+
+if __name__ == "__main__":
+    main()
