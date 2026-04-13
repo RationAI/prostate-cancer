41b607023db9b81d157862b3786232d8f5b53d89
Adam Džadoň <524839@mail.muni.cz>
2026-04-13 07:59:20 +0000

    feat: merging script

---
+++ preprocessing/embeddings/merge_embeddings.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+
+import hydra
+import mlflow
+import pandas as pd
+import torch
+from omegaconf import DictConfig
+from rationai.mlkit import autolog, with_cli_args
+from rationai.mlkit.lightning.loggers import MLFlowLogger
+from rationai.tiling.writers import save_mlflow_dataset
+
+
+def merge_embeddings(
+    slides: pd.DataFrame, tiles: pd.DataFrame, embeddings_dir: Path, name: str
+) -> pd.DataFrame:
+    col = f"{name}_embedding"
+    tiles[col] = None
+
+    for _, slide in slides.iterrows():
+        slide_name = Path(slide.path).stem
+
+        embeds = torch.load(
+            (embeddings_dir / slide_name).with_suffix(".pt"), map_location="cpu"
+        ).numpy()
+        mask = tiles["slide_id"] == slide.id
+
+        if mask.sum() != len(embeds):
+            raise ValueError(
+                f"Mismatch for slide {slide.id}: {mask.sum()} tiles vs {len(embeds)} embeddings"
+            )
+
+        # to avoid pandas treating the arrays as multi-column scalars
+        idx = tiles.index[mask]
+        tiles.loc[mask, col] = pd.Series(list(embeds), index=idx)
+
+    return tiles
+
+
+@with_cli_args(["+preprocessing=merge_embeddings"])
+@hydra.main(config_path="../configs", config_name="preprocessing", version_base=None)
+@autolog
+def main(config: DictConfig, logger: MLFlowLogger) -> None:
+    tiling_path = mlflow.artifacts.download_artifacts(config.data.tiles_uri_224)
+
+    virchow2_embeds_dir = (
+        Path(mlflow.artifacts.download_artifacts(config.data.virchow2_embeddings_uri))
+        if config.data.pgp_embeddings_uri is not None
+        else None
+    )
+    pgp_embeds_dir = (
+        Path(mlflow.artifacts.download_artifacts(config.data.pgp_embeddings_uri))
+        if config.data.pgp_embeddings_uri is not None
+        else None
+    )
+    slides = pd.read_parquet(tiling_path + "/slides.parquet")
+    tiles = pd.read_parquet(tiling_path + "/tiles.parquet")
+
+    if virchow2_embeds_dir is not None:
+        tiles = merge_embeddings(slides, tiles, virchow2_embeds_dir, "virchow2")
+
+    if pgp_embeds_dir is not None:
+        tiles = merge_embeddings(slides, tiles, pgp_embeds_dir, "pgp")
+
+    save_mlflow_dataset(slides, tiles, config.data.data_name + "_with_embeddings")
