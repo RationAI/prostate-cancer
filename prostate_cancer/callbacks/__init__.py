b6b9f92bde7ba2671a3fd9b150a4fb3a7a21af1e
Adam Džadoň <524839@mail.muni.cz>
2026-04-21 10:06:01 +0200

    Experiment/heurstic aggregation (#10)
    * feat: preprocessing apply template
    
    * feat: data split
    
    * feat: tissue masks
    
    * feat: tissue masks
    
    * fix: typo
    
    * fix: relative path
    
    * feat: annot masks
    
    * feat: qc masks
    
    * feat: stats
    
    * feat: rest of preprocessing configs
    
    * fix: typo
    
    * feat: merge some embedding configs
    
    * feat: thresholds
    
    * fix: data name
    
    * fix: remove old stuff
    
    * feat: use case run name
    
    * fix: data prefix
    
    * feat: postprocessing template apply
    
    * feat: init postprocessing experiment structure
    
    * fix: typos
    
    * fix: typo
    
    * feat: postprocesing experiments and enhanced model data
    
    * feat: postprocessing testing
    
    * fix: merge table
    
    * fix: mypy
    
    * chore: catch up working branch
    
    * feat: catchup ml
    
    * feat: topk catchup
    
    * feat: add top k inference for each model
    
    * feat: eval estimation configs
    
    * feat: slide level curves
    
    * fix: remove additional configs
    
    * chore: test postprocessing configs
    
    * fix: remove embeddings prefix
    
    * feat: span estimation experiments
    
    * fix: remove uris prfix
    
    * fix: missing span cell threshold
    
    * fix: typo
    
    * feat: span pg
    
    * feat: span inference
    
    * feat: init slide level eval
    
    * feat: improve evaluation
    
    * feat: eval virchow
    
    * feat: slide eval val part
    
    * feat: corrected span predictions
    
    * feat: resolve agent comments

--- prostate_cancer/callbacks/__init__.py
+++ prostate_cancer/callbacks/__init__.py
@@ -4,10 +4,10 @@ from prostate_cancer.callbacks.carcinoma_prediction_table_callback import (
     CarcinomaPredictionTableCallback,
 )
 from prostate_cancer.callbacks.curves_callback import CurvesCallback
-from prostate_cancer.callbacks.heatmap_callback import HeatmapCallback
-from prostate_cancer.callbacks.kernel_estimation_callback import (
-    KernelEstimationCallback,
+from prostate_cancer.callbacks.estimation_callback import (
+    EstimationCallback,
 )
+from prostate_cancer.callbacks.heatmap_callback import HeatmapCallback
 from prostate_cancer.callbacks.nested_metrics_callback import NestedMetricsCallback
 from prostate_cancer.callbacks.num_positive_callback import NumPositiveCallback
 from prostate_cancer.callbacks.tile_histograms_callback import TileHistogramsCallback
@@ -18,8 +18,8 @@ __all__ = [
     "CAMExplainer",
     "CarcinomaPredictionTableCallback",
     "CurvesCallback",
+    "EstimationCallback",
     "HeatmapCallback",
-    "KernelEstimationCallback",
     "NestedMetricsCallback",
     "NumPositiveCallback",
     "TileHistogramsCallback",
