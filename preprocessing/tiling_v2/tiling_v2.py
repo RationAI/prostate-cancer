3261e9a3c2f85e7092853b463a6ecebb000fbea3
Adam Džadoň <524839@mail.muni.cz>
2026-05-04 07:36:22 +0200

    Experiment/panda (#11)
    * feat: metadata
    
    * feat: radboud split
    
    * feat: add names
    
    * feat: radboud split
    
    * feat: drop unused columns
    
    * feat: new uri
    
    * fix: correct axis
    
    * fix: use current metadata
    
    * fix: has prefix
    
    * feat: add new metadata
    
    * feat: tt split
    
    * fix: lint
    
    * feat: val split
    
    * fix: resolve comments
    
    * feat: update uris
    
    * feat: public artifact storage
    
    * feat: panda tiling mask
    
    * feat: panda msk
    
    * fix: format
    
    * fix: format
    
    * feat: qc masks
    
    * feat: unify artifacts storage
    
    * feat: switch to the public infra
    
    * feat: update level qc
    
    * feat: tiling v2 init
    
    * fix: reading csv
    
    * fix: path
    
    * feat: proper expansion
    
    * fix: downloading uris
    
    * fix: typo
    
    * fix: typo
    
    * fix: add batch
    
    * fix: typo
    
    * fix: use mask name
    
    * chore: debug print
    
    * fix: typo
    
    * fix: typo
    
    * fix: avoid Nones
    
    * chore: temporarily remove carcinoma
    
    * feat: apply qc findings
    
    * feat: empty paths
    
    * feat: try missing slides
    
    * feat: handle missing masks
    
    * feat: dummy masks
    
    * fix: pass fallback
    
    * fix: ray timing
    
    * feat: untie script
    
    * fix: path to config
    
    * fix: correct name
    
    * feat: tiling radboud
    
    * fix: typing
    
    * fix: masks name
    
    * fix: comments
    
    * feat: overlappers general
    
    * fix: ray scope
    
    * feat: generalize roi
    
    * feat: correct merge
    
    * feat: comments
    
    * fix: format
    
    * fix: mypy
    
    * fix: format
    
    * feat: tile masks
    
    * fix: name
    
    * fix: add tile uris
    
    * fix: bump tile masks
    
    * chore: debug
    
    * chore: debug
    
    * feat: neatpicks
    
    * feat: add test config
    
    * feat: minimal cexample
    
    * feat: back to dicts
    
    * feat: specific downloads
    
    * feat: bump concurency
    
    * feat: avoid per file download
    
    * feat: remove num cpus
    
    * feat:  remove memory est
    
    * feat: use old tiling
    
    * fix: add placeholder annotation
    
    * feat: old tiling tm
    
    * feat: slide mpp4
    
    * fix: config
    
    * feat: new mpp masks
    
    * feat: new tiles
    
    * fix: lint
    
    * fix: remove unused config
    
    * fix: add context manager untie masks

---
+++ preprocessing/tiling_v2/tiling_v2.py
@@ -0,0 +1,140 @@
+from pathlib import Path
+from typing import TYPE_CHECKING, Any
+
+import hydra
+import mlflow
+import pandas as pd
+import pyvips
+import ray
+from lightning.pytorch.loggers import Logger
+from omegaconf import DictConfig
+from rationai.masks import write_big_tiff
+from rationai.mlkit import with_cli_args
+from rationai.mlkit.autolog import autolog
+from rationai.tiling.writers import save_mlflow_dataset
+from ratiopath.ray import read_slides
+from ratiopath.tiling import grid_tiles
+from ratiopath.tiling.utils import row_hash
+
+
+if TYPE_CHECKING:
+    from preprocessing.tiling_v2.overlappers import Overlapper
+
+
+def create_dummy_wsi(
+    path: Path, mpp_x: float = 0.25, mpp_y: float = 0.25, tile_size: int = 512
+) -> Path:
+    image = pyvips.Image.black(tile_size, tile_size).bandjoin([0, 0])
+    image = image.cast("uchar")
+
+    write_big_tiff(
+        image=image,
+        path=path,
+        mpp_x=mpp_x,
+        mpp_y=mpp_y,
+        tile_width=tile_size,
+        tile_height=tile_size,
+    )
+    return path
+
+
+def tile(row: dict[str, Any]) -> list[dict[str, Any]]:
+    """Factory tile generator."""
+    return [
+        {
+            "tile_x": x,
+            "tile_y": y,
+            "path": row["path"],
+            "slide_id": row["id"],
+            "level": row["level"],
+            "tile_extent_x": row["tile_extent_x"],
+            "tile_extent_y": row["tile_extent_y"],
+            "mpp_x": row["mpp_x"],
+            "mpp_y": row["mpp_y"],
+        }
+        for x, y in grid_tiles(
+            slide_extent=(row["extent_x"], row["extent_y"]),
+            tile_extent=(row["tile_extent_x"], row["tile_extent_y"]),
+            stride=(row["stride_x"], row["stride_y"]),
+        )
+    ]
+
+
+def select(row: dict[str, Any], to_keep: set[str]) -> dict[str, Any]:
+    """Filter tiles to contain only desired columns."""
+    tile = {
+        "slide_id": row["slide_id"],
+        "x": row["tile_x"],
+        "y": row["tile_y"],
+    }
+
+    for column in to_keep:
+        tile[column] = row[column]
+
+    return tile
+
+
+def carcinoma(row: dict[str, Any], df: pd.DataFrame) -> dict[str, Any]:
+    """Add slide level carcinoma label to slides table."""
+    slide_path = row["path"]
+    match = df.loc[df["slide_path"] == slide_path, "carcinoma"]
+
+    if match.empty:
+        raise ValueError("No matching carcinoma label")
+
+    row["carcinoma"] = match.iloc[0]
+    return row
+
+
+def tiling(
+    df: pd.DataFrame, config: DictConfig, fallback: str
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    overlappers: list[Overlapper] = list(
+        hydra.utils.instantiate(config.overlappers).values()
+    )
+
+    slides = read_slides(
+        list(df["slide_path"]),
+        tile_extent=config.tile_extent,
+        stride=config.stride,
+        mpp=config.mpp,
+    )
+    slides = slides.map(row_hash)
+    slides = slides.map(carcinoma, fn_kwargs={"df": df})
+
+    tiles = slides.flat_map(tile)
+    tiles = tiles.repartition(target_num_rows_per_block=config.batch_size)
+
+    to_keep = set()
+    for overlapper in overlappers:
+        tiles = overlapper.add_mask_path(tiles, fallback)
+        tiles = overlapper.add_percentages(tiles)
+        tiles = overlapper.filter(tiles)
+        to_keep |= overlapper.columns_to_keep
+
+    tiles = tiles.map(select, fn_kwargs={"to_keep": to_keep})
+
+    return slides.to_pandas(), tiles.to_pandas()
+
+
+@with_cli_args(["+preprocessing=tiling_v2"])
+@hydra.main(config_path="../../configs", config_name="preprocessing", version_base=None)
+@autolog
+def main(config: DictConfig, logger: Logger | None = None) -> None:
+    fallback = (
+        Path(".") / "dummy_wsi.tiff"
+    )  # not using tempfile since I encountered strange behaviour with ray
+    create_dummy_wsi(fallback)
+    df = pd.read_csv(mlflow.artifacts.download_artifacts(config.data.metadata_table))
+
+    ctx = ray.data.DataContext.get_current()
+    ctx.enable_rich_progress_bars = True
+    ctx.use_ray_tqdm = False
+    with ray.init(runtime_env={"excludes": [".git", ".venv"]}):  # type: ignore[call-arg]
+        slides, tiles = tiling(df, config, str(fallback))
+        save_mlflow_dataset(slides, tiles, config.data.data_name)
+        fallback.unlink()
+
+
+if __name__ == "__main__":
+    main()
