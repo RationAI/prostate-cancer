3b28c21ae322e23c947776288c4ea2ce71cead16
Ubuntu <adam.dzadon@gmail.com>
2026-05-18 12:18:14 +0000

    feat: vit training

--- prostate_cancer/modeling/backbone/google_vit.py
+++ prostate_cancer/modeling/backbone/google_vit.py
@@ -2,4 +2,6 @@ from transformers import ViTModel
 
 
 def google_vit() -> ViTModel:
-    return ViTModel.from_pretrained("google/vit-base-patch16-224", add_pooling_layer=False)
+    return ViTModel.from_pretrained(
+        "google/vit-base-patch16-224", add_pooling_layer=False
+    )
